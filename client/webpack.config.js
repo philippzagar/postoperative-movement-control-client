a0c4e4b79d70c04284d707fd253b6342c636ff37
Philipp1999 <philipp.zagar@gmail.com>
2017-11-07 09:13:41 +0100

    Overall update of project

---
+++ client/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+      entry: './src/app.js',
+      output: {
+          path: path.join(__dirname, 'public'),
+          filename: 'bundle.js'
+      },
+      module: {
+          rules: [{
+              loader: 'babel-loader',
+              test: /\.js$/,
+              exclude: /node_modules/
+          }]
+      },
+      devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+      devServer: {
+          contentBase: path.join(__dirname, 'public')
+      }
+  };
+};
